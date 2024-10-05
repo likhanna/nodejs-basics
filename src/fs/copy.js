80886576e6abf496c101a708af0f5ce8fa7b4989
likhanna <khlr@list.ru>
2024-10-05 20:44:02 +0500

    create & copy done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import path from "path";
+import { cp } from "fs/promises";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const srcFolder = path.join(__dirname, "files");
+  const destFolder = path.join(__dirname, "files_copy");
+
+  try {
+    await cp(srcFolder, destFolder, {
+      recursive: true,
+      force: false,
+      errorOnExist: true,
+    });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { promises as fs } from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = `${__dirname}/files/fresh.txt`;
+  const data = "I am fresh and young";
+
+  try {
+    await fs.writeFile(filePath, data, { flag: "wx" });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
