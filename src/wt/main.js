4e28ca4be6fc48c67c0a936ccec84e56156e4d20
likhanna <khlr@list.ru>
2024-10-13 19:19:59 +0500

    fix worker issue

--- src/wt/main.js
+++ src/wt/main.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const workerDest = path.join(__dirname, "files", "worker.js");
+const workerDest = path.join(__dirname, "worker.js");
 
 const performCalculations = async () => {
   let increment = 10;
